7a44384e0fea003c4d896a690b6df014613b0b8d
Zydak <2137.wielki.polak@gmail.com>
2025-07-19 09:17:30 +0200

    Writing pages to RAM

--- Include/SSD1306Driver.h
+++ Include/SSD1306Driver.h
@@ -1,20 +1,35 @@
 #pragma once
 
+#include <driver/i2c_master.h>
 #include "esp_err.h"
 
 #include <vector>
+#include <array>
 
 class SSD1306Driver
 {
 public:
-    int Foo();
+    struct SSD1306DriverConfiguration
+    {
+        i2c_port_num_t I2CPort = I2C_NUM_0;
+        gpio_num_t SdaIO = GPIO_NUM_10;
+        gpio_num_t SclIO = GPIO_NUM_11;
+        uint32_t I2CSclSpeedHz = 400000;
+        bool FlipRendering = false;
+        bool InvertColors = false;
+    };
+
+    SSD1306Driver(const SSD1306DriverConfiguration& configuration);
 
     esp_err_t FlushCommandBuffer();
 
     //
     // --------------- All raw commands ---------------
     //
-    // For more info on the commands consult the chapter 9 command table at https://cdn-shop.adafruit.com/datasheets/SSD1306.pdf#
+    // For more info on the commands consult the chapter 9 (command table) in the data sheet https://cdn-shop.adafruit.com/datasheets/SSD1306.pdf#
+
+    void AppendControlByteCommand();
+    void AppendControlByteData();
 
     // Fundamental Commands
     esp_err_t AppendSetContrastControl(uint8_t contrast);
@@ -39,9 +54,9 @@ public:
 
     // Hardware Configuration Commands
     esp_err_t AppendSetDisplayStartLine(uint8_t line);
-    esp_err_t AppendSetSegmentRemap(bool remap);
+    esp_err_t AppendSetSegmentRemap(bool remapLeftToRight);
     esp_err_t AppendSetMultiplexRatio(uint8_t ratio);
-    esp_err_t AppendSetComOutputScanDirection(bool remap);
+    esp_err_t AppendSetComOutputScanDirection(bool remapTopToBottom);
     esp_err_t AppendSetDisplayOffset(uint8_t offset);
     esp_err_t AppendSetComPins(bool alternative, bool remap);
 
@@ -50,7 +65,34 @@ public:
     esp_err_t AppendSetPreChargePeriod(uint8_t phase1, uint8_t phase2);
     esp_err_t AppendSetVComHDeselectLevel(uint8_t level);
     esp_err_t AppendNOP();
+
+    esp_err_t WritePageToRam(uint8_t page);
+    esp_err_t WriteAllPagesToRam();
+
+    esp_err_t WriteToPage(uint8_t page, void* data, uint8_t size, uint8_t offset);
 private:
+
+    class Pages
+    {
+    public:
+        const uint8_t* GetPagePtr(uint8_t page);
+        esp_err_t WritePage(uint8_t page, void* data, uint8_t size, uint8_t offset);
+
+        bool HasPageChanged(uint8_t page);
+        void PageSentToRam(uint8_t page);
+    private:
+        #define PAGES_COUNT 8
+        #define PAGE_SIZE 129
+
+        // Store everything in a single continous buffer so it's more memory effiecient
+        std::array<uint8_t, PAGE_SIZE * PAGES_COUNT> Buffer; // 129 because the first byte in each page is 0x40 (control data byte) and the rest 128 are columns
+        uint8_t HavePagesChanged = 0; // Bit field
+    };
+
+    Pages m_Pages;
+
+    i2c_master_bus_handle_t m_I2CBusHandle = nullptr;
+    i2c_master_dev_handle_t m_I2CHandle = nullptr;
 
     // You usually want to send multiple commands at once, so all commands are saved
     // to the command buffer and sent with FlushCommandBuffer()
--- Source/SSD1306Driver.cpp
+++ Source/SSD1306Driver.cpp
@@ -2,11 +2,83 @@
 
 #include "esp_log.h"
 
+#include <freertos/FreeRTOS.h>
+
+#include <cstring>
+
 #define TAG "SSD1306Driver"
 
-int SSD1306Driver::Foo()
+SSD1306Driver::SSD1306Driver(const SSD1306DriverConfiguration &configuration)
 {
-    return 5;
+    i2c_master_bus_config_t busConfig = {};
+    busConfig.clk_source = I2C_CLK_SRC_DEFAULT;
+    busConfig.glitch_ignore_cnt = 7;
+    busConfig.i2c_port = configuration.I2CPort;
+    busConfig.scl_io_num = configuration.SclIO;
+    busConfig.sda_io_num = configuration.SdaIO;
+    busConfig.flags.enable_internal_pullup = true;
+
+    ESP_ERROR_CHECK(i2c_new_master_bus(&busConfig, &m_I2CBusHandle));
+    ESP_ERROR_CHECK(i2c_master_probe(m_I2CBusHandle, 0x3C, 5000 / portTICK_PERIOD_MS));
+    i2c_device_config_t I2CDeviceConfig = {
+        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        .device_address = 0x3C,
+        .scl_speed_hz = configuration.I2CSclSpeedHz
+    };
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(m_I2CBusHandle, &I2CDeviceConfig, &m_I2CHandle));
+
+    m_CommandBuffer.reserve(256);
+
+    // Reset SSD1306 RAM
+    std::array<uint8_t, 128> nullData;
+    nullData.fill(0);
+    for (int i = 0; i < PAGES_COUNT; i++)
+    {
+        m_Pages.WritePage(i, nullData.data(), 128, 0);
+    }
+    WriteAllPagesToRam();
+
+    // Reset Everything according to data sheet command table https://cdn-shop.adafruit.com/datasheets/SSD1306.pdf
+    AppendControlByteCommand();
+    AppendSetDisplayOnOff(false);
+    AppendSetContrastControl(0x7F);
+    AppendEntireDisplayOn(false);
+    AppendSetNormalInverseDisplay(configuration.InvertColors);
+    AppendDeactivateScroll();
+    AppendSetLowerColumnStartAddress(0);
+    AppendSetHigherColumnStartAddress(0);
+    AppendSetMemoryAddressingMode(0b10);
+    AppendSetPageStartAddress(0);
+    AppendSetDisplayStartLine(0);
+    AppendSetSegmentRemap(configuration.FlipRendering);
+    AppendSetMultiplexRatio(0b00111111);
+    AppendSetComOutputScanDirection(configuration.FlipRendering);
+    AppendSetDisplayOffset(0);
+    AppendSetComPins(true, false);
+    AppendSetDisplayClockDivideRatioAndOscillatorFrequency(0b0000, 0b1000);
+    AppendSetPreChargePeriod(0x2, 0x2);
+    AppendSetVComHDeselectLevel(0b010);
+    AppendSetDisplayOnOff(true);
+
+    FlushCommandBuffer();
+}
+
+esp_err_t SSD1306Driver::FlushCommandBuffer()
+{
+    esp_err_t ret = i2c_master_transmit(m_I2CHandle, m_CommandBuffer.data(), m_CommandBuffer.size(), 1000 / portTICK_PERIOD_MS);
+    m_CommandBuffer.clear();
+
+    return ret;
+}
+
+void SSD1306Driver::AppendControlByteCommand()
+{
+    m_CommandBuffer.emplace_back(0x00);
+}
+
+void SSD1306Driver::AppendControlByteData()
+{
+    m_CommandBuffer.emplace_back(0x40);
 }
 
 esp_err_t SSD1306Driver::AppendSetContrastControl(uint8_t contrast)
@@ -241,18 +313,18 @@ esp_err_t SSD1306Driver::AppendSetDisplayStartLine(uint8_t line)
     return ESP_OK;
 }
 
-esp_err_t SSD1306Driver::AppendSetSegmentRemap(bool remap)
+esp_err_t SSD1306Driver::AppendSetSegmentRemap(bool remapLeftToRight)
 {
-    m_CommandBuffer.emplace_back(0b10100000 | remap);
+    m_CommandBuffer.emplace_back(0b10100000 | remapLeftToRight);
     
     return ESP_OK;
 }
 
 esp_err_t SSD1306Driver::AppendSetMultiplexRatio(uint8_t ratio)
 {
-    if (ratio >= 64)
+    if (ratio >= 64 || ratio <= 14)
     {
-        ESP_LOGE(TAG, "Ratio must be between 0 and 63! It currently is %d", (int)ratio);
+        ESP_LOGE(TAG, "Ratio must be between 15 and 63! It currently is %d", (int)ratio);
         return ESP_FAIL;
     }
 
@@ -262,9 +334,9 @@ esp_err_t SSD1306Driver::AppendSetMultiplexRatio(uint8_t ratio)
     return ESP_OK;
 }
 
-esp_err_t SSD1306Driver::AppendSetComOutputScanDirection(bool remap)
+esp_err_t SSD1306Driver::AppendSetComOutputScanDirection(bool remapTopToBottom)
 {
-    m_CommandBuffer.emplace_back(0b11000000 | (remap << 3));
+    m_CommandBuffer.emplace_back(0b11000000 | (remapTopToBottom << 3));
     
     return ESP_OK;
 }
@@ -312,12 +384,12 @@ esp_err_t SSD1306Driver::AppendSetDisplayClockDivideRatioAndOscillatorFrequency(
 
 esp_err_t SSD1306Driver::AppendSetPreChargePeriod(uint8_t phase1, uint8_t phase2)
 {
-    if (phase1 >= 16 && phase1 != 0)
+    if (phase1 >= 16 || phase1 == 0)
     {
         ESP_LOGE(TAG, "Phase1 must be between 0 and 15 and can't be 0! It currently is %d", (int)phase1);
         return ESP_FAIL;
     }
-    if (phase2 >= 16 && phase2 != 0)
+    if (phase2 >= 16 || phase2 == 0)
     {
         ESP_LOGE(TAG, "Phase2 must be between 0 and 15 and can't be 0! It currently is %d", (int)phase2);
         return ESP_FAIL;
@@ -331,9 +403,9 @@ esp_err_t SSD1306Driver::AppendSetPreChargePeriod(uint8_t phase1, uint8_t phase2
 
 esp_err_t SSD1306Driver::AppendSetVComHDeselectLevel(uint8_t level)
 {
-    if (level == 0x00 || level == 0x10 || level == 0x20)
+    if (level != 0b000 && level != 0b010 && level != 0b011)
     {
-        ESP_LOGE(TAG, "level has to be either 0x00, 0x10 or 0x20! It currently is %x", (int)level);
+        ESP_LOGE(TAG, "level has to be either 0b000, 0b010 or 0b011! It currently is %d", (int)level);
         return ESP_FAIL;
     }
 
@@ -341,4 +413,105 @@ esp_err_t SSD1306Driver::AppendSetVComHDeselectLevel(uint8_t level)
     m_CommandBuffer.emplace_back(level << 6);
 
     return ESP_OK;
+}
+
+esp_err_t SSD1306Driver::AppendNOP()
+{
+    m_CommandBuffer.emplace_back(0b11100011);
+
+    return ESP_OK;
+}
+
+esp_err_t SSD1306Driver::WritePageToRam(uint8_t page)
+{
+    // Most of the time there's no need for updating the entire screen, so the HasChanged flag notes any changes to pages
+    // and if there are none skip the writing
+    if (!m_Pages.HasPageChanged(page))
+        return ESP_OK;
+    
+    if (m_CommandBuffer.size() > 0)
+    {
+        ESP_LOGE(TAG, "You have to flush the command buffer before writing to RAM!");
+        return ESP_FAIL;
+    }
+
+    ESP_LOGI(TAG, "Writing page %d to RAM", page);
+
+    AppendControlByteCommand();
+    esp_err_t res = AppendSetPageStartAddress(page); // Page bounds (0-7) are checked here so there's no need for another if statement
+    if (res != ESP_OK)
+        return res;
+
+    res = FlushCommandBuffer();
+    if (res != ESP_OK)
+        return res;
+    
+    esp_err_t ret = i2c_master_transmit(m_I2CHandle, m_Pages.GetPagePtr(page), PAGE_SIZE, 1000 / portTICK_PERIOD_MS);
+    m_Pages.PageSentToRam(page);
+
+    return ret;
+}
+
+esp_err_t SSD1306Driver::WriteAllPagesToRam()
+{
+    for (int i = 0; i < PAGES_COUNT; i++)
+    {
+        esp_err_t res = WritePageToRam(i);
+        if (res != ESP_OK)
+            return res;
+    }
+
+    return ESP_OK;
+}
+
+esp_err_t SSD1306Driver::WriteToPage(uint8_t page, void *data, uint8_t size, uint8_t offset)
+{
+    return m_Pages.WritePage(page, data, size, offset);
+}
+
+const uint8_t *SSD1306Driver::Pages::GetPagePtr(uint8_t page)
+{
+    if (page >= 8)
+    {
+        ESP_LOGE(TAG, "Page has to be between 0 and 7! It currently is %d", (int)page);
+        return nullptr;
+    }
+
+    return Buffer.data() + (page * PAGE_SIZE);
+}
+
+esp_err_t SSD1306Driver::Pages::WritePage(uint8_t page, void *data, uint8_t size, uint8_t offset)
+{
+    if (page >= 8)
+    {
+        ESP_LOGE(TAG, "Page has to be between 0 and 7! It currently is %d", (int)page);
+        return ESP_FAIL;
+    }
+    if (size + offset > 128)
+    {
+        ESP_LOGE(TAG, "Size + Offset has to be between 0 and 128! It currently is %d", int(size + offset));
+        return ESP_FAIL;
+    }
+
+    // + 1 because first byte in the Columns is the control data byte (0x40) not the actual data
+    uint8_t* pagePtr = Buffer.data() + (page * PAGE_SIZE);
+
+    pagePtr[0] = 0x40; // Set the control byte
+    memcpy(pagePtr + 1 + offset, data, size);
+
+    // Set page bit to 1
+    HavePagesChanged = HavePagesChanged | (1 << page);
+
+    return ESP_OK;
+}
+
+bool SSD1306Driver::Pages::HasPageChanged(uint8_t page)
+{
+    return (HavePagesChanged & (1 << page)) > 0;
+}
+
+void SSD1306Driver::Pages::PageSentToRam(uint8_t page)
+{
+    // Set page bit to 0
+    HavePagesChanged = HavePagesChanged & ~(1 << page);
 }
